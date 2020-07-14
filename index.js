210b7ae7d0bb83e1435f0a98befed0e7ab6a52b9
rock43053 <agm.baf@gmail.com>
2020-07-14 11:09:06 -0400

    Complete

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-function calculate(numberOne, operation, numberTwo) {
+function calculate(numberOne, operation, numberTwo){
+    if (numberOne === undefined){
+        numberOne = 0
+    
+    }
+    if (numberTwo === undefined){
+        numberTwo = 0
+    }
+    switch (operation){
+        case '+':
+            return parseInt(numberOne) + parseInt(numberTwo)
+            break;
+        case '-':
+            return parseInt(numberOne) - parseInt(numberTwo)
+            break;
+        case '*':
+            return parseInt(numberOne) * parseInt(numberTwo)
+            break;
+        case '/':
+            return parseInt(numberOne) / parseInt(numberTwo)
+            break;
+        default:
+            return ' we are currently not supporting this ' + operation
+    }
+
+
+
+
+
 }
 
+//console.log(calculate(undefined, '^', '10'))
 /**
  Export an object with two properties.
  One property is `calculate`. The value will be the calculate function.
